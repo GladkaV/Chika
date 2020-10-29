f675a638919f0aa609211f70a152fabd6f164545
GladkaV <vktr.gldk10.98@gmail.com>
2020-10-30 00:01:45 +0200

    added menu-list

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,13 +11,20 @@ $(function () {
     $('.header__content-item.user').click(function () {
         $('.header__user-list').toggleClass('active');
     })
-    
+
     $(document).mouseup(function (e) { 
         if (!$(".header__content-item.user").is(e.target) && 
             $(".header__content-item.user").has(e.target).length === 0) { 
             $(".header__user-list").removeClass('active');
         }
     });
+
+    // menu list btn
+    $('.menu-down .menu-list__btn').click(function() {
+        $(this).parent().toggleClass('active');
+        $(this).toggleClass('active');
+        $(this).next().slideToggle();
+    })
        
     
 
